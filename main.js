4becba7daf8dfd75a9011404c509d226ff8205d6
siddhant pandey <siddhant_pandey.scsebca@galgotiasuniversity.edu.in>
2021-10-22 17:14:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,99 +9,89 @@ const newQuoteBtn = document.getElementById('new-quote');
 const favBtn = document.getElementById('favorite');
 const lsOutput = document.getElementById('lsoutput');
 const loader = document.getElementById('loader');
-const dev = '@chowkidarnot';
+const dev = '@siddhant4real';
 
 // show loading
 
-function loading(){
-    loader.hidden = false;
-    quoteContainer.hidden = true; // show our loader and hide our quote container
-    
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true; // show our loader and hide our quote container
 }
 
 // hide loader
 
-function complete(){
-    if(!loader.hidden) { // false
-        quoteContainer.hidden = false;
-        loader.hidden = true;
+function complete() {
+  if (!loader.hidden) {
+    // false
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+async function getQuote() {
+  loading();
+
+  // using a proxy API to fix CORS issue
+
+  const apiUrl = 'https://api.quotable.io/random';
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data.content);
+    // if author is blank
+
+    if (data.author === '') {
+      authorText.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.author;
     }
-}
 
-async function getQuote(){
-    loading();
+    // request fontsize for long text
 
-    // using a proxy API to fix CORS issue
-    const proxyURL = 'https://cors-anywhere.herokuapp.com/';
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    
-    try{
-        const response = await fetch(proxyURL + apiUrl); // combine these two
-        const data = await response.json();
-
-        // if author is blank
-
-        if(data.quoteAuthor === ''){
-            authorText.innerText = 'Unknown';
-        }else{
-            authorText.innerText = data.quoteAuthor;
-        }
-
-        // request fontsize for long text
-
-        if(data.quoteText.length > 120){
-            quoteText.classList.add('long-quote');
-        }else{
-            quoteText.classList.remove('long-quote');
-        }
-
-        quoteText.innerText = data.quoteText; // data coming from api
-
-        // stop loader , show quote
-        complete();
-
-    }catch(e){
-        getQuote();
-        
-        
+    if (data.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
     }
+
+    quoteText.innerText = data.content; // data coming from api
+
+    // stop loader , show quote
+    complete();
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-function tweetQuote(){
-    const quote = quoteText.innerText;
-    const author = authorText.innerText;
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author} - ${dev} `;
-    window.open(twitterUrl, '_blank');
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author} - ${dev}(developer) `;
+  window.open(twitterUrl, '_blank');
 }
 
+function addfav() {
+  const key = quoteText.innerText;
+  const value = authorText.innerText;
 
-function addfav(){
-    const key = quoteText.innerText;
-    const value = authorText.innerText;
-    
-    if(key && value){
-        localStorage.setItem(key,value);
-        location.reload();
-    }
+  if (key && value) {
+    localStorage.setItem(key, value);
+    location.reload();
+  }
 }
 
+for (let i = 0; i < localStorage.length; i++) {
+  const key = localStorage.key(i);
+  const value = localStorage.getItem(key);
 
-
-for(let i = 0; i < localStorage.length; i++){
-    
-    const key = localStorage.key(i);
-    const value = localStorage.getItem(key);
-
-    lsOutput.innerHTML += `${key} : ${value}<br /> <br />`;
-    
+  lsOutput.innerHTML += `${key} : ${value}<br /> <br />`;
 }
-
 
 //Event Listners
-newQuoteBtn.addEventListener('click',getQuote);
-twitterBtn.addEventListener('click',tweetQuote);
-favBtn.addEventListener('click',addfav);
-
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+favBtn.addEventListener('click', addfav);
 
 // on load
 getQuote();
